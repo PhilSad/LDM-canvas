439bebcd62f4fb18da1d91fe976a9928ddcc4e38
Philippe Saade <filouface@gmail.com>
2022-08-19 16:55:32 +0000

    script to class

--- access_model.py
+++ access_model.py
@@ -32,89 +32,6 @@ import clip
 
 # argument parsing
 
-parser = argparse.ArgumentParser()
-
-parser.add_argument('--model_path', type=str, default = 'finetune.pt',
-                   help='path to the diffusion model')
-
-parser.add_argument('--kl_path', type=str, default = 'kl-f8.pt',
-                   help='path to the LDM first stage model')
-
-parser.add_argument('--bert_path', type=str, default = 'bert.pt',
-                   help='path to the LDM first stage model')
-
-parser.add_argument('--text', type = str, required = False, default = '',
-                    help='your text prompt')
-
-parser.add_argument('--edit', type = str, required = False,
-                    help='path to the image you want to edit (either an image file or .npy containing a numpy array of the image embeddings)')
-
-parser.add_argument('--edit_x', type = int, required = False, default = 0,
-                    help='x position of the edit image in the generation frame (need to be multiple of 8)')
-
-parser.add_argument('--edit_y', type = int, required = False, default = 0,
-                    help='y position of the edit image in the generation frame (need to be multiple of 8)')
-
-parser.add_argument('--edit_width', type = int, required = False, default = 0,
-                    help='width of the edit image in the generation frame (need to be multiple of 8)')
-
-parser.add_argument('--edit_height', type = int, required = False, default = 0,
-                    help='height of the edit image in the generation frame (need to be multiple of 8)')
-
-parser.add_argument('--mask', type = str, required = False,
-                    help='path to a mask image. white pixels = keep, black pixels = discard. width = image width/8, height = image height/8')
-
-parser.add_argument('--negative', type = str, required = False, default = '',
-                    help='negative text prompt')
-
-parser.add_argument('--init_image', type=str, required = False, default = None,
-                   help='init image to use')
-
-parser.add_argument('--skip_timesteps', type=int, required = False, default = 0,
-                   help='how many diffusion steps are gonna be skipped')
-
-parser.add_argument('--prefix', type = str, required = False, default = '',
-                    help='prefix for output files')
-
-parser.add_argument('--num_batches', type = int, default = 1, required = False,
-                    help='number of batches')
-
-parser.add_argument('--batch_size', type = int, default = 1, required = False,
-                    help='batch size')
-
-parser.add_argument('--width', type = int, default = 256, required = False,
-                    help='image size of output (multiple of 8)')
-
-parser.add_argument('--height', type = int, default = 256, required = False,
-                    help='image size of output (multiple of 8)')
-
-parser.add_argument('--seed', type = int, default=-1, required = False,
-                    help='random seed')
-
-parser.add_argument('--guidance_scale', type = float, default = 5.0, required = False,
-                    help='classifier-free guidance scale')
-
-parser.add_argument('--steps', type = int, default = 0, required = False,
-                    help='number of diffusion steps')
-
-parser.add_argument('--cpu', dest='cpu', action='store_true')
-
-parser.add_argument('--clip_score', dest='clip_score', action='store_true')
-
-parser.add_argument('--clip_guidance', dest='clip_guidance', action='store_true')
-
-parser.add_argument('--clip_guidance_scale', type = float, default = 150, required = False,
-                    help='Controls how much the image should look like the prompt') # may need to use lower value for ddim
-
-parser.add_argument('--cutn', type = int, default = 16, required = False,
-                    help='Number of cuts')
-
-parser.add_argument('--ddim', dest='ddim', action='store_true') # turn on to use 50 step ddim
-
-parser.add_argument('--ddpm', dest='ddpm', action='store_true') # turn on to use 50 step ddim
-
-args = parser.parse_args()
-
 def fetch(url_or_path):
     if str(url_or_path).startswith('http://') or str(url_or_path).startswith('https://'):
         r = requests.get(url_or_path)
@@ -243,8 +160,16 @@ class Access_Model:
         self.clip_model, self.clip_preprocess = clip.load('ViT-L/14', device=self.device, jit=False)
         self.clip_model.eval().requires_grad_(False)
         self.normalize = transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711])
-
-    def do_run(args):
+        
+        if args.ddpm:
+            self.sample_fn = self.diffusion.ddpm_sample_loop_progressive
+        elif args.ddim:
+            self.sample_fn = self.diffusion.ddim_sample_loop_progressive
+        else:
+            self.sample_fn = self.diffusion.plms_sample_loop_progressive
+        
+        
+    def do_run(self, args):
         gc.collect()
 
         print("DEBUG - Starting do_run")
@@ -401,12 +326,7 @@ class Access_Model:
 
                 return -torch.autograd.grad(loss, x)[0]
     
-        if args.ddpm:
-            sample_fn = self.diffusion.ddpm_sample_loop_progressive
-        elif args.ddim:
-            sample_fn = self.diffusion.ddim_sample_loop_progressive
-        else:
-            sample_fn = self.diffusion.plms_sample_loop_progressive
+
 
         def save_sample(i, sample, clip_score=False):
             for k, image in enumerate(sample['pred_xstart'][:args.batch_size]):
@@ -447,7 +367,7 @@ class Access_Model:
         for i in range(args.num_batches):
             cur_t = self.diffusion.num_timesteps - 1
 
-            samples = sample_fn(
+            samples = self.sample_fn(
                 model_fn,
                 (args.batch_size*2, 4, int(args.height/8), int(args.width/8)),
                 clip_denoised=False,
