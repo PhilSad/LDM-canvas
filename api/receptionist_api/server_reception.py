63c8c96ec7292218dc26ddd59fa719de432de454
philsad <filouface@gmail.com>
2022-08-22 12:13:42 +0000

    fix

--- api/receptionist_api/server_reception.py
+++ api/receptionist_api/server_reception.py
@@ -1,24 +1,20 @@
-from turtle import width
 from flask import Flask, send_file
 from flask import request
 import base64
 import requests
 import sys
 import os
+import json
 
 from PIL import Image, ImageDraw
-sys.path.append('../imagen_api/models_bindings/')
-from dalle_mini_mega import DalleGenerator
 from flask_cors import CORS
 
-URL_IMAGEN_SERVER = 'http://35.206.191.68/'
+URL_IMAGEN_SERVER = 'http://35.206.191.68:5000/imagine'
 
 
 app = Flask(__name__)
 CORS(app)
-print('bbbb')
-generator = DalleGenerator()
-print('aaaa')
+
 
 @app.route("/full_canvas/")
 def full_canvas():
@@ -27,20 +23,23 @@ def full_canvas():
 
 @app.route("/imagine/")
 def imagine():
-
-    b64prompt = request.args.get('prompt')
+    prompt = request.args.get('prompt')
     posX = int(request.args.get('posX'))
     posY = int(request.args.get('posY'))
     width = int(request.args.get('width'))
     height = int(request.args.get('height'))
     
-    url_with_params = f'{URL_IMAGEN_SERVER}:5000?prompt={prompt}&posX={posX}&posY={posY}&width={width}&height={height}' 
-
-    response = requests.post(url_with_params)
+    url_with_params = f'{URL_IMAGEN_SERVER}?prompt={prompt}&posX={posX}&posY={posY}&width={width}&height={height}' 
+    print(url_with_params)
+    response = requests.get(url_with_params)
+    print(response.text)
+    generated = json.loads(response.text)
 
     canvas = Image.open('./images/full_canvas.png')
     canvas.paste(generated, (posX, posY))
     canvas.save('./images/full_canvas.png', quality=100)
 
     return 'ok'
+
+
 
