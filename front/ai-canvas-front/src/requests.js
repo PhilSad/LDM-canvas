a011d7d2cd7f552e62bdecb4c23622386cbc4bfc
PhilSad <filouface@gmail.com>
2022-09-27 23:49:16 +0200

    post credential to api

--- front/ai-canvas-front/src/requests.js
+++ front/ai-canvas-front/src/requests.js
@@ -1,6 +1,12 @@
 let URL_REGISTER_IF_NOT_EXIST = "https://function-register-if-not-exist-jujlepts2a-ew.a.run.app/"
 
-function send_connexion_request (credential){
+export function send_connexion_request (credential){
     console.log('sending register request')
-    fetch(URL_REGISTER_IF_NOT_EXIST + "?credential=" + credential)
+    fetch(URL_REGISTER_IF_NOT_EXIST, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({'credential' : credential}),
+      })
 }
