6758e103cbd6f00caab2ca2d82a5954784987e3a
root <root@instance-gpu-preemptible.europe-west1-b.c.ai-canvas.internal>
2022-09-29 20:27:14 +0000

    added flask for ew image gen

--- api/imagen_api/server_sd_pipe.py
+++ api/imagen_api/server_sd_pipe.py
@@ -6,8 +6,10 @@ from io import BytesIO
 import base64
 import torch
 import PIL
+import numpy as np
 
 from diffusionui import StableDiffusionPipeline
+
 
 storage_client = storage.Client()
 bucket = storage_client.bucket('aicanvas-public-bucket')
@@ -25,22 +27,52 @@ pipe = StableDiffusionPipeline.from_pretrained(
     use_auth_token=True
 ).to(device)
 
-# pipe.disable_nsfw_filter()
+pipe.disable_nsfw_filter()
 
 MAX_SIZE = 512
+STEPS = 50
+
+def get_mask(im):
+    im = im.split()[-1].convert('1')
+    return PIL.ImageOps.invert(im)
+
+def get_noise(im):
+    n = np.asarray(diffuse(prompt='', height=im.height, width=im.width, steps=1)[0])
+        
+    m = get_mask(im)
+
+    # get masks and invert mask
+    ma = np.asarray(m)
+    ma = np.array([[[p] * 3 for p in r ] for r in ma])
+    ma_i = np.asarray(PIL.ImageOps.invert(m))
+    ma_i = np.array([[[p] * 3 for p in r ] for r in ma_i])
+
+    # convert to rgb to keep same dimension
+    ima = np.asarray(im.convert('RGB'))
+
+    # add noise and original image
+    final = ((ma_i + n) * ma) + (ima * ma_i)
+
+    return PIL.Image.fromarray(final, mode="RGB")
 
 def adjust_size(width,height):
     ratio = width/height
     if(ratio > 1):
         width = MAX_SIZE
-        height = ((round(MAX_SIZE * 1/ratio) + 32) // 64) * 64
-        height= int(max(height, 64))
+        round_width = width
+
+        height = round(MAX_SIZE * 1/ratio)
+        round_height = ((height + 32) // 64) * 64
+        round_height = int(max(round_height, 64))
     else:
         height = MAX_SIZE
-        width = ((round(MAX_SIZE * ratio) + 32) // 64) * 64
-        width = int(max(width, 64))
+        round_height = height
 
-    return width, height
+        width = round(MAX_SIZE * ratio) 
+        round_width = ((width + 32) // 64) * 64
+        round_width = int(max(round_width, 64))
+
+    return width, height, round_width, round_height,
 
 def diffuse(prompt, n_images=1, width=512, height=512, steps=50, init_image=None, mask_image=None, guidance_scale=7.5, strength=0.8, generator=None):
     from torch import autocast
@@ -59,23 +91,27 @@ def diffuse(prompt, n_images=1, width=512, height=512, steps=50, init_image=None
 
     return generated
 
-@app.route("/new_image/")
+@app.route("/new_image/", methods=['POST'])
 def new_image():
-    b64prompt = request.args.get('prompt')
-    width = int(request.args.get('width'))
-    height = int(request.args.get('height'))
+    params = request.get_json()
 
-    width, height = adjust_size(width, height)
+    b64prompt = params['prompt']
+    width = int(params['width'])
+    height = int(params['height'])
+
+    width, height, round_width, round_height = adjust_size(width, height)
 
     prompt = base64.b64decode(b64prompt)
     prompt = prompt.decode("utf-8")
 
     generated = diffuse(
             prompt=prompt,
-            height=height,
-            width=width,
-            steps=25
+            height=round_height,
+            width=round_width,
+            steps=50
             )[0]
+
+    generated = generated.resize((width, height), PIL.Image.ANTIALIAS)
 
     # save to cloud
     storage_client = storage.Client()
@@ -96,41 +132,43 @@ def inpaint_alpha():
     b64prompt = params['prompt']
     width = int(params['width'])
     height = int(params['height'])
-    
     init_image= params['init_image']
+
+    width, height, round_width, round_height = adjust_size(width, height)
 
     # decode base64 image
     im = PIL.Image.open(BytesIO(base64.b64decode(init_image)))
     im = im.crop((0, 0, width, height))
-
-    # get alpha channel as mask, and convert it to black and white
-    mask = PIL.ImageOps.invert(im.split()[-1].convert('1'))
-    mask = mask.crop((0, 0, width, height))
-
-    width, height = adjust_size(width, height)
+    im = im.resize((round_width, round_height), PIL.Image.ANTIALIAS)
+    im = im.convert('RGBA')
 
     prompt = base64.b64decode(b64prompt)
     prompt = prompt.decode("utf-8")
+    
+    noise = get_noise(im)
+    mask = get_mask(im)
 
     generated = diffuse(
             prompt=prompt,
-            height=height,
-            width=width,
-            init_image=im.convert('RGB'),
-            mask_image=mask.convert('RGB'),
-            steps=25
+            width=round_width,
+            height=round_height,
+            init_image=noise,
+            mask_image=mask,
+            steps=STEPS
             )[0]
+
+    generated = generated.resize((width, height), PIL.Image.ANTIALIAS)
 
     # save to cloud
     storage_client = storage.Client()
+    buffered = BytesIO()
+
+    generated.save(buffered, format="JPEG")
     bucket = storage_client.bucket('aicanvas-public-bucket')
 
     # todo save image
-
-    buffered = BytesIO()
-    generated.save(buffered, format="JPEG")
+    
     img_str = base64.b64encode(buffered.getvalue())
-
     return Response(img_str, status=200)
 
 @app.route("/inpaint_mask/", methods=['POST'])
@@ -162,7 +200,7 @@ def inpaint_mask():
             width=width,
             init_image=im,
             mask_image=mask,
-            steps=25
+            steps=STEPS
             )[0]
 
     # save to cloud
@@ -173,6 +211,8 @@ def inpaint_mask():
 
     buffered = BytesIO()
     generated.save(buffered, format="JPEG")
+    generated.save('tmp.jpeg')
+
     img_str = base64.b64encode(buffered.getvalue())
 
     return Response(img_str, status=200)
