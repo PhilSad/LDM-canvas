13108f34cd82f4229fda3b9c310fd3ad1ef58a7a
PhilSad <filouface@gmail.com>
2022-09-30 21:49:02 +0200

    pubsub for all functions

--- api/imagen_api/pubsub-gpu.py
+++ api/imagen_api/pubsub-gpu.py
@@ -1,10 +1,17 @@
 
 from concurrent.futures import TimeoutError
 from google.cloud import pubsub_v1
+from google.cloud import storage
 import json
 import requests
-
+import datetime
 import imagen_lib as imagen
+import base64
+import os
+from dotenv import load_dotenv
+import sqlalchemy as db
+import datetime
+
 
 project_id = "732264051436"
 subscription_id = "imagen-queue-sub"
@@ -14,8 +21,69 @@ subscriber = pubsub_v1.SubscriberClient()
 subscription_path = subscriber.subscription_path(project_id, subscription_id)
 
 
+load_dotenv()
 
-URL_IMAGEN = "localhost:5000"
+db_user = os.environ["DB_USER"]  # e.g. 'my-database-user'
+db_pass = os.environ["DB_PASS"]  # e.g. 'my-database-password'
+db_name = os.environ["DB_NAME"]  # e.g. 'my-database'
+db_ip = os.environ["DB_IP"]  # private address
+
+storage_client = storage.Client()
+bucket = storage_client.bucket('aicanvas-public-bucket')
+
+
+
+def connect_unix_socket() -> db.engine.base.Engine:
+    pool = db.create_engine(
+        url = db.engine.url.URL.create(
+            drivername="mysql+pymysql",
+            username=db_user,
+            password=db_pass,
+            host=db_ip,
+            port=3306,
+            database=db_name,
+        ))
+    return pool
+
+def getTable(name, engine):
+    metadata = db.MetaData()
+    return db.Table(name, metadata, autoload=True, autoload_with=engine)
+
+
+def save_to_sql(data_to_add):
+    # save image to db
+    engine = connect_unix_socket()
+    images = getTable('images', engine)
+
+    query = db.insert(images).values(**data_to_add)
+    engine.execute(query)
+
+
+def save_to_bucket(save_path, generated, bucket_path):
+    generated.save(save_path, quality=50)
+    blob_path_save_image = bucket.blob(bucket_path)
+    blob_path_save_image.upload_from_filename(save_path)
+
+
+
+def push_to_clients(channel, data):
+    APPSYNC_API_ENDPOINT_URL = "https://jsnbrfwmpfdkjnhocqnrnuibbq.appsync-api.us-east-1.amazonaws.com/graphql"
+
+    query = """
+        mutation Publish($data: AWSJSON!, $name: String!) {
+            publish(data: $data, name: $name) {
+                data
+                name
+            }
+        }"""
+
+    jsonData=json.dumps(data)
+    variables = json.dumps(dict(name=channel, data=jsonData))
+
+    headers = {'x-api-key' : "da2-xpnjchk6vbdfdi2qovntvm6fcq"}
+
+    response = requests.post(APPSYNC_API_ENDPOINT_URL, json={'query': query, 'variables' : variables}, headers=headers)
+    print(response.text)
 
 
 def callback(message: pubsub_v1.subscriber.message.Message) -> None:
@@ -23,12 +91,54 @@ def callback(message: pubsub_v1.subscriber.message.Message) -> None:
     data = json.loads(message.data)
 
     action = data['action']
-    method = data['method']
-    url_params  = data['url_params']
-    post_params = data['post_params']
+    params = data['params']
 
-    if action == 'new_image':
-        imagen.new_image(post_params)
+    # todo send ws with cur image uuid
+
+    if   action == 'new_image':
+        generated = imagen.new_image(**params)
+    
+    elif action == 'img_to_img':
+        generated = imagen.image_to_image(**params)
+
+    elif action == 'inpaint_alpha':
+        generated = imagen.inpaint_alpha(**params)
+    
+    elif action == 'inpaint_mask':
+        generated = imagen.inpaint_mask(**params)
+    
+    else:
+        return
+
+    b64prompt = params['prompt']
+    canvas_width = int(params['width'])
+    canvas_height = int(params['height'])
+    posX = int(params['posX'])
+    posY = int(params['posY'])
+    room = params['room']
+
+    prompt = base64.b64decode(b64prompt)
+    prompt = prompt.decode("utf-8")
+    
+    ts = str(datetime.datetime.now().strftime("%Y%m%d%H%M%S%f"))
+    bucket_path = f'{room}/{ts}-{prompt}.jpeg'
+
+    data_to_add = dict(
+        path=bucket_path,
+        posX=posX,
+        posY=posY,
+        width = canvas_width,
+        height = canvas_height,
+        prompt = prompt
+    )
+
+    save_to_sql(data_to_add)
+
+    save_path = f'/tmp/{ts}.jpeg'
+    save_to_bucket(save_path, generated, bucket_path)
+
+    push_to_clients(room, data_to_add)
+
 
     message.ack()
 
