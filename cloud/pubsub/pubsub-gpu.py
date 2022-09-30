9a69a38da78a638985995ac153f849c50c9a9ea2
PhilSad <filouface@gmail.com>
2022-09-30 16:05:07 +0200

    pubsub receiver

--- cloud/pubsub/pubsub-gpu.py
+++ cloud/pubsub/pubsub-gpu.py
@@ -2,6 +2,7 @@
 from concurrent.futures import TimeoutError
 from google.cloud import pubsub_v1
 import json
+import requests
 
 project_id = "732264051436"
 subscription_id = "imagen-queue-sub"
@@ -13,16 +14,30 @@ subscriber = pubsub_v1.SubscriberClient()
 # in the form `projects/{project_id}/subscriptions/{subscription_id}`
 subscription_path = subscriber.subscription_path(project_id, subscription_id)
 
+
+
+URL_IMAGEN = "https://gpu.apipicaisso.ml"
+
+
 def callback(message: pubsub_v1.subscriber.message.Message) -> None:
     print(f"Received {message}.")
     data = json.loads(message.data)
+
     action = data['action']
+    method = data['method']
+    url_params  = data['url_params']
+    post_params = data['post_params']
 
-    if action == 'new image':
-        pass
+    formated_url_params = '&'.join(f'{k}={v}' for k,v in url_params.items())
+    formated_url = f'{URL_IMAGEN}/{action}/?{formated_url_params}'
+    
+    print(formated_url)
+    print(post_params)
 
-
+    resp = requests.request(method, formated_url, json=post_params)
+    print(resp)
     message.ack()
+
 
 streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
 print(f"Listening for messages on {subscription_path}..\n")
@@ -33,7 +48,7 @@ with subscriber:
         # When `timeout` is not set, result() will block indefinitely,
         # unless an exception is encountered first.
         streaming_pull_future.result()
-    except TimeoutError:
+    except :
         streaming_pull_future.cancel()  # Trigger the shutdown.
         streaming_pull_future.result()  # Block until the shutdown is complete.
 
