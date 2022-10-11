c86cac54b044e115ea381cb75be2d5f5a4d0175f
Ruben <rubengres@hotmail.fr>
2022-10-12 01:11:36 +0200

    oops i pushed in prod again

--- front/ai-canvas-front/src/InfiniteCanvas.js
+++ front/ai-canvas-front/src/InfiniteCanvas.js
@@ -48,6 +48,8 @@ const InfiniteCanvas = (props) => {
 
     return (
         <>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+            
             <MyCanvas
                 camera={camera}
                 modifiers={modifiers}
