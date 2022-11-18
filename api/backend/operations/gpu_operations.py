4082e9e989b680a7eedc36a9a2590a12289311d4
philsad <filouface@gmail.com>
2022-11-18 01:10:44 +0000

    fix

--- api/backend/operations/gpu_operations.py
+++ api/backend/operations/gpu_operations.py
@@ -16,10 +16,11 @@ credentials, project_id = google.auth.default()
 
 def start_vm_if_not_started():
     """return boolean true if vm was already started """
-    result = compute.instances().get(project='ai-canvas', zone='us-central1-a', instance='template-imagen-gpu-auto-1').execute()
     if hasattr(credentials, "service_account_email"):
         print('[DEBUG] SERVICE ACCOUNT')
         print(credentials.service_account_email)
+
+    result = compute.instances().get(project='ai-canvas', zone='us-central1-a', instance='template-imagen-gpu-auto-1').execute()
 
 
     if result["status"] != "RUNNING":
